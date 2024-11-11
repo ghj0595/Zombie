5fad2ea5458543b75c54da68bf54f2cd03f0be0a
ghj0595 <ghj0595@naver.com>
2024-11-11 16:06:32 +0900

    Add .Unit fields and death()

--- Zombie/src/zombie/Unit.java
+++ Zombie/src/zombie/Unit.java
@@ -1,14 +1,26 @@
 package zombie;
 
 abstract public class Unit implements Attackable {
+	private final int END = 0;
+
+	protected String name;
 	protected int hp;
 	protected int power;
 	protected int position;
-	
-	public Unit(int hp, int power, int position) {
+
+	public Unit(String name, int hp, int power, int position) {
+		this.name = name;
 		this.hp = hp;
 		this.power = power;
 		this.position = position;
+	}
+
+	protected void deth() {
+		if (this.hp <= END) {
+			this.hp = END;
+			String message = String.format("[%s가 죽었습니다.]", this.name);
+			System.out.println(message);
+		}
 	}
 
 }
