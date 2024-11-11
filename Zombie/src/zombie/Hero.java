9c175333054026a63f9fbc505a45265af3cf1a01
ghj0595 <ghj0595@naver.com>
2024-11-11 16:22:29 +0900

    Add .Hero fields, attack() and healing()

--- Zombie/src/zombie/Hero.java
+++ Zombie/src/zombie/Hero.java
@@ -1,5 +1,51 @@
 package zombie;
 
-public class Hero {
+import java.util.Random;
+
+public class Hero extends Unit {
+	private final int EMPTY = 0;
+	private final int LUCK = 3;
+	private final int BASIC = 5;
+	private final int RECOVERY = 100;
+	private final int MAX_HP;
+
+	private Random random = new Random();
+
+	private int potion;
+
+	public Hero(String name, int hp, int power, int position) {
+		super(name, hp, power, position);
+		this.MAX_HP = hp;
+		this.potion = BASIC;
+	}
+
+	@Override
+	public void attack(Unit unit) {
+		int number = random.nextInt(5);
+
+		if (number == LUCK) {
+			unit.hp -= (this.power * 2);
+		} else {
+			unit.hp -= this.power;
+		}
+	}
+
+	public void healing() {
+		if (potion == EMPTY) {
+			System.out.println("포션이 부족합니다.");
+			return;
+		}
+
+		if (this.hp == MAX_HP) {
+			System.out.println("HP가 가득 찼습니다.");
+		} else if (this.hp < MAX_HP) {
+			this.hp += RECOVERY;
+			if (this.hp > MAX_HP) {
+				this.hp = MAX_HP;
+			}
+			System.out.printf("HP를 회복했습니다. [%d/%d]\n", this.hp, this.MAX_HP);
+			potion--;
+		}
+	}
 
 }
