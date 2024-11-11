79c46b150ff6a43a64daac7f0866d3735eba12e5
ghj0595 <ghj0595@naver.com>
2024-11-11 17:13:25 +0900

    Add .Boss fields and attack()

--- Zombie/src/zombie/Boss.java
+++ Zombie/src/zombie/Boss.java
@@ -1,5 +1,25 @@
 package zombie;
 
-public class Boss {
+import java.util.Random;
+
+public class Boss extends Zombie {
+	private final int HIDDEN = 3;
+	
+	private Random random = new Random();	
+
+	public Boss(String name, int hp, int power, int position) {
+		super(name, hp, power, position);
+	}
+	
+	@Override
+	public void attack(Unit unit) {
+		super.attack(unit);
+		int skil = random.nextInt(5);
+		
+		if(skil == HIDDEN) {
+			unit.hp -= unit.hp / 2;
+			System.out.printf("BOSS의 스킬이 적중했습니다! %s[%d/%d]\n", unit.name, unit.hp, unit.MAX_HP);
+		}
+	}
 
 }
