eb53d0c4707e7e84fe4164de2037923ff756ba9d
ghj0595 <ghj0595@naver.com>
2024-11-11 16:58:22 +0900

    Add .Zombie fields ands attack()

--- Zombie/src/zombie/Zombie.java
+++ Zombie/src/zombie/Zombie.java
@@ -1,7 +1,19 @@
 package zombie;
 
-public class Zombie {
+abstract class Zombie extends Unit {
 
+	protected final int BLOODSUCKING;
 
+	public Zombie(String name, int hp, int power, int position) {
+		super(name, hp, power, position);
+		this.BLOODSUCKING = power / 10;
+	}
+
+	@Override
+	public void attack(Unit unit) {
+		unit.hp -= this.power;
+		this.hp += this.BLOODSUCKING;
+		System.out.printf("%s가 생명력을 회복했습니다.[%d/%d]", this.name, this.hp, this.MAX_HP);
+	}
 
 }
