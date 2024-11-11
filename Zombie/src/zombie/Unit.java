17fd410ec4f121d0376dc5ebf99223f12f6e20ee
ghj0595 <ghj0595@naver.com>
2024-11-11 15:27:38 +0900

    Add .Unit protected fields

--- Zombie/src/zombie/Unit.java
+++ Zombie/src/zombie/Unit.java
@@ -1,5 +1,14 @@
 package zombie;
 
-public class Unit {
+abstract public class Unit implements Attackable {
+	protected int hp;
+	protected int power;
+	protected int position;
+	
+	public Unit(int hp, int power, int position) {
+		this.hp = hp;
+		this.power = power;
+		this.position = position;
+	}
 
 }
