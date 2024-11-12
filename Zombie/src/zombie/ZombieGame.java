feccce914bb9369099e8d9ff5c7f45ffbc39131f
ghj0595 <ghj0595@naver.com>
2024-11-12 10:38:47 +0900

    Modify Zombie Game fields, heroDeath(), recoveryHeroHp() and Add .Zombie Game slow()

--- Zombie/src/zombie/ZombieGame.java
+++ Zombie/src/zombie/ZombieGame.java
@@ -4,81 +4,111 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ZombieGame {
-	private final int RECOVERY = 1;
 	private final int END = 0;
 	private Scanner scanner = new Scanner(System.in);
 	private Random random = new Random();
-	
+
 	private static ZombieGame instance = new ZombieGame();
-	
+
 	public static ZombieGame getInstance() {
 		return instance;
 	}
-	
-	private Hero hero;
+
 	private Zombie zombie;
+	private Hero hero;
 	private Boss boss;
+	private double warnig;
 	private boolean fight;
 	private boolean isRun = true;
-	
+
 	public void run() {
 		setGame();
-		while(true){		
-			play();
+		while (isRun) {
+			play();			
 		}
 	}
+
 	private void setGame() {
 		String name = input("Hero 이름 입력");
-		hero = new Hero(name, 150, 10, 1);
-		
+		hero = new Hero(name, 200, 10, 1);
+		warnig = hero.MAX_HP * 0.7;
+
 		zombie = new Zombie("Zombie", 50, 10, 5);
-		boss = new Boss("BIG BOSS", 500, 50, 10);
+		boss = new Boss("BIG BOSS", 500, 30, 10);
 	}
-	
+
 	private void play() {
-		hero.position ++;
-		
-		if(hero.position == zombie.position) {
-			System.out.println("좀비와 마주쳤습니다!");			
-			while(!fight) {
-				fightZombie();
-				if(hero.hp == END) {
-					isRun = false;
-					System.out.println("GAME OVER 영웅이 죽었습니다...");
-				}
-			}
+		hero.position++;
+
+		if (hero.position == zombie.position) {
+			System.out.println("좀비와 마주쳤습니다!");
+			encounterZombie(zombie);
+		}
+
+		if (hero.position == boss.position) {
+			System.out.println("보스 등장!!!");
+			encounterZombie(boss);
 		}
 	}
-	
-	private void fightZombie() {
-		hero.attack(zombie);
-		
-		if(zombie.death()) {
-			fight = true;
+
+	private void encounterZombie(Unit unit) {
+		fight = true;
+		while (fight) {
+			fightZombie(unit);
+			if (heroDeath()) {
+				break;
+			}
+			recoveryHeroHp();
+		}
+	}
+
+	private boolean heroDeath() {
+		if (hero.hp <= END) {
+			hero.hp = END;
+			isRun = false;
+			System.out.println("GAME OVER 영웅이 죽었습니다...");
+			return true;
+		}
+		return false;
+	}
+
+	private void fightZombie(Unit unit) {
+		hero.attack(unit);
+
+		if (unit.death()) {
+			fight = false;
 			return;
 		}
-		printUnitHp(zombie);
-		
-		zombie.attack(hero);
+		printUnitHp(unit);
+
+		unit.attack(hero);
 		printUnitHp(hero);
 		
-		int number = random.nextInt(2);
-		if(number == RECOVERY) {
+		slow();
+	}
+
+	private void recoveryHeroHp() {
+		if (hero.hp < warnig) {
 			hero.healing();
 		}
+
 	}
-	
+
 	private void printUnitHp(Unit unit) {
 		System.out.printf("%s HP : [%d/%d]\n", unit.name, unit.hp, unit.MAX_HP);
 	}
-	
+
 	private String input(String message) {
 		System.out.println(message + " : ");
 		String name = scanner.nextLine();
 		return name;
 	}
 	
-	
-
+	private void slow() {
+		try {
+			Thread.sleep(600);
+		} catch (Exception e) {		
+		}
+	}
 
 }
